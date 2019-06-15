ae1d262101415f2b05de155bf93fa3d0eb89b23c
Nate Wang <pwang7@ebay.com>
2019-06-15 23:08:31 +0800

    Clean site.

--- public/styles-b571e0345a96dd42cf9e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{103:function(n,o,c){},104:function(n,o,c){},144:function(n,o,c){},146:function(n,o,c){},147:function(n,o,c){},148:function(n,o,c){}}]);
-//# sourceMappingURL=styles-b571e0345a96dd42cf9e.js.map
--- src/components/Banner.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Banner from './Banner';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Banner />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
